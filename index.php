49fad8b11781d86fc8645b043ba78c0c81c14ba5
oddib <60390653+oddib@users.noreply.github.com>
2020-01-28 13:42:56 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv='refresh' content='0; URL=http://www.novasoftware.se/ImgGen/schedulegenerator.aspx?format=pdf&schoolid=60870/nn-no&type=3&id={71F78AB4-0D6B-47AC-A7FD-7FEDBB72F956}&period=
+	&week=<?php $week = date('W')+0;
+	echo $week;?>
+	&mode=0&printer=0&colors=32&head=0&clock=0&foot=0&day=0&width=960&height=1209&maxwidth=960&maxheight=1209'>
+</head>
+
+<?php $week = date('W')+0;
+echo $week;?>
+</body>
+</html>
